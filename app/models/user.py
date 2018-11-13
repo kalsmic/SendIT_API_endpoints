dbb8901008bb648350bb00545541763c137b1474
Kalule Arthur <kalulearthur@gmail.com>
2018-11-13 08:40:09 +0300

    Added create a new parcel order endpoint [#161805007] [#161804940]  On branch create_a_parcel  Changes to be committed: 	modified:   app/__init__.py 	modified:   requirements.txt 	new file:   test/test_create_new_parcel_delivery_order.py

---
+++ app/models/user.py
@@ -0,0 +1,55 @@
+# user.py
+""" Module contains endpoints for getting a parcel order for a given user"""
+
+from flask import (
+    jsonify,
+    request,
+    Blueprint
+)
+
+from app.dummy_data import (
+    PARCELS,
+    USERS
+)
+from app.http_responses import (
+    Not_found,
+    Bad_request
+)
+
+user = Blueprint('user',__name__,url_prefix='/api/v1/users')
+
+@user.route('/<userId>/parcels', methods=['GET'])
+def get_a_parcel_by_userId(userId):
+    """Fetch all parcel delivery
+    orders by a specific user """
+
+    try:
+        userId = int(userId)
+
+    # id is not of type Number
+    except (TypeError, ValueError):
+        return jsonify(Bad_request), 400
+
+    # Is User valid
+    user_exists = False
+    for user in USERS:
+        if user['userId'] == userId:
+            user_exists = True
+
+    if not user_exists:
+        return jsonify(Bad_request), 400
+
+    # Only look up parcels for a valid user
+    user_parcels = []
+    for parcel in PARCELS:
+        if parcel['ownerId'] == userId:
+
+            user_parcels.append(parcel)
+
+        # valid user has no parcels
+    if not user_parcels:
+        return jsonify(Not_found), 404
+    # valid user has parcels
+    return jsonify({'parcels': user_parcels}), 200
+
+    
--- test/test_get_a_parcel.py
+++ test/test_get_a_parcel.py
@@ -1,4 +1,3 @@
-
 from flask import request, url_for, json
 from app.errors import (
     Not_found
